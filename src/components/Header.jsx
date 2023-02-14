64e434b9b40a19ce34cf55d0a34951a78ed8cbb6
diegocejas08 <diegondiaz@hotmail.com>
2023-02-13 22:02:42 -0300

    Se agrega proyecto

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+
+const Header = () => {
+    return (
+        <>
+
+            <header>
+                <Navbar />
+                <h1 className='text-4xl font-bold uppercase'>Desde Header</h1>
+            </header>
+        </>
+    )
+}
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+  content: ["./public/index.html", "./src/**/*.jsx"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
