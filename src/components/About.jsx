017d815f6a43d30cb7f85714bf73c1488f457cf9
IsaChiquin <isachiquindl@gmail.com>
2023-02-21 11:32:58 -0300

    se modifica el About quitandole el Menu y se agrega el Contact

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import menu from './About-img/combo1.webp';
-import falafel from './About-img/paraiso.webp';
+import paraiso from './About-img/paraiso.webp';
 import franq from './About-img/franquicia.webp';
 
 
 const About = () => {
     return (
-        <><section className='mt-40 mb-80 h-80 bg-greenveg md:mb-48 grid place-content-center'>
-        <div className='w-9/12 mx-auto md:max-w-full md:flex'>
-            <div className='md:w-3/5 flex-none'>
-                <img className='rounded-full' src={menu} alt="" />
-            </div>
-            <div className='bg-white -mt-10 border-2 border-greenveg place-self-center p-4 grid place-content-center md:w-96 md:h-full md:border-0'>
-                <div className='mb-8'>
-                    <div className='text-center font-questrial'>
-                        <h2 className='text-gray-800 font-bold mt-8 text-3xl md:w-96 md:text-3xl'>El menú mas apetitoso</h2>
-                        <h2 className='font-yellowtail text-gray-800 text-4xl mb-6 md:mb-14'>en un click</h2>
-                        <button className='bg-greenveg h-10 w-28 text-white text-2xl font-bold rounded shadow-lg hover:scale-110 hover:bg-orangeveg'>MENU</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </section>
-        <section className='mb-96 h-80 bg-orangeveg md:mb-52 grid place-content-center'>
+        <><section className='mt-40 mb-80 h-80 bg-orangeveg md:mb-60 grid place-content-center'>
                 <div className='w-9/12 mx-auto md:w-8/12 md:flex'>
                     <div className='bg-white -mb-10 border-2 border-orangeveg place-self-center p-4 grid place-content-center md:h-full md:w-96 md:border-0'>
                         <div className='mb-8'>
@@ -30,14 +13,16 @@ const About = () => {
                                 <h2 className='font-yellowtail text-gray-800 text-3xl mb-2 md:text-5xl md:mb-8'>El paraiso del Falafel</h2>
                                 <h3 className='mb-4 font-questrial text-gray-800 text-2xl md:mb-8'>¿Ya probaste nuestros platos?</h3>
                                 <p className='font-questrial md:leading-6'>Disfrutá de una mezcla de sabores inigualables elaborados a base de especias, granos y un sinfín de vegetales como pimiento, cebolla, tomate, garbanzos, berenjenas.</p>
+                                <img src="" alt="" />
                             </div>
                         </div>
                     </div>
                     <div className='md:w-3/5 flex-none'>
-                        <img className='rounded-full' src={falafel} alt="" />
+                        <img className='rounded-full' src={paraiso} alt="" />
                     </div>
                 </div>
-            </section><section className='mb-48 h-80 bg-greenveg grid place-content-center'>
+            </section>
+            <section className='mb-60 h-80 bg-greenveg grid place-content-center md:mb-52'>
                 <div className='w-9/12 mx-auto md:w-8/12 md:flex'>
                     <div className='md:w-3/5 flex-none'>
                         <img className='rounded-full' src={franq} alt="" />
@@ -53,7 +38,8 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-            </section></>
+            </section>
+            </>
         )
     };
 
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import falafel from './About-img/falafel.webp';
 
 const Contact = () => {
     return (
-        <div>Contact</div>
+        <div className='bg-orangeveg md:grid grid-rows-5 grid-cols-5 content-center w-3/4 mx-auto p-10'>
+                <div className='col-span-2 row-span-4 justify-self-center self-center'>
+                    <h2 className='text-white font-questrial text-4xl font-bold mb-10 text-center md:mb-14'>¡Contáctanos!</h2>
+                    <p className='text-white font-questrial text-2xl mb-3 text-center md:text-3xl'>¿Querés hablar con nosotros?</p>
+                    <p className='text-white font-questrial text-2xl mb-8 text-center md:text-3xl'>Comentanos tus dudas, sugerencias y más.</p>
+                </div>
+                <div className='mb-8 col-span-2 row-span-1 col-start-1 row-start-5 justify-self-center self-center md:mb-0'>
+                    <img className='rounded-xl justify-self-center object-cover h-20 w-72' src={falafel} alt="" />
+                </div>
+                <div className='bg-white row-span-5 col-span-3 col-start-3 rounded-xl p-8 shadow-lg text-gray-800'>
+                    <form action="" className='flex flex-col space-y-4 md:place-self-end font-questrial'>
+                        <div>
+                            <label htmlFor="" className='text-lg' >Nombre</label>
+                            <input type="text" placeholder='Tu nombre' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-orangeveg'/>
+                        </div>
+                        <div>
+                            <label htmlFor="" className='text-lg' >Email</label>
+                            <input type="email" placeholder='Tu Email' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-orangeveg'/>
+                        </div>
+                        <div>
+                            <label htmlFor="" className='text-lg' >Mensaje</label>
+                            <textarea rows='4' placeholder='Escribe aqui tu mensaje' className='ring-1 ring-gray-300 w-full rounded-md px-4 py-2 outline-none focus:ring-2 focus:ring-orangeveg'>
+                            </textarea>
+                        </div>
+                        <button className='inline-block self-end bg-orangeveg text-white font-bold rounded-lg px-6 py-2 hover:bg-greenveg'>Enviar mensaje</button>
+                    </form>
+                </div>
+            </div>
     )
 }
 
