3514684ba546e6382413c7e2acd1986d7fd8d63c
IsaChiquin <isachiquindl@gmail.com>
2023-03-14 18:40:58 -0300

    se agrega css a carrito

--- src/Carrito/ItemCarrito.jsx
+++ src/Carrito/ItemCarrito.jsx
@@ -3,13 +3,29 @@ import React from 'react'
 const ItemCarrito =({data, deleteFromCart}) => {
     const {id, nombre, precio, cantidad}= data;
   return (
-    <div>
-        <h5 className='font-questrial'> {nombre}- $ {precio}- {cantidad}- Subtotal= ${precio*cantidad} </h5>
-        <button onClick={()=>deleteFromCart(id, false)}   className='bg-greenveg h-10 w-28 text-white text-2xl font-bold rounded shadow-lg hover:scale-110 hover:bg-orangeveg font-questrial'>Eliminar</button>
-        <button onClick={()=> deleteFromCart(id, true)}      className='bg-orangeveg h-10 w-28 text-white  font-bold rounded shadow-lg hover:scale-110 hover:bg-orangeveg font-questrial'>Eliminar Todo </button>
-
-        
+    <><table className='font-questrial divide-y divide-x divide-solid mt-6'>
+      <thead>
+        <tr>
+          <th className='w-32'>Nombre</th>
+          <th className='w-20'>Precio</th>
+          <th className='w-20'>Cantidad</th>
+          <th className='w-24'>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody className='text-center h-8'>
+        <tr>
+          <td>{nombre}</td>
+          <td>${precio}</td>
+          <td>{cantidad}</td>
+          <td>${precio * cantidad}</td>
+        </tr>
+      </tbody>
+    </table>
+    <div className='flex mb-4'>
+        <button onClick={() => deleteFromCart(id, false)} className='bg-greenveg h-8 w-28 text-white font-bold rounded shadow-lg hover:scale-110 hover:bg-orangeveg font-questrial'>Eliminar</button>
+        <button onClick={() => deleteFromCart(id, true)} className='bg-orangeveg h-8 w-28 text-white font-bold rounded shadow-lg hover:scale-110 hover:bg-orangeveg font-questrial'>Eliminar Todo </button>
     </div>
+    </>
   )
 }
 export default ItemCarrito;
